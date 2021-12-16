f6d68890a4ca84c0866070e0c4d6c68b79b95a2e
Siobhán Sterrett <siobhan.sterrett@gmail.com>
2021-12-15 19:36:50 -0500

    Ditches terrain and difficulty enums

--- src/hex.rs
+++ src/hex.rs
@@ -2,8 +2,6 @@ use serde::{Deserialize, Serialize};
 
 use iced::{Point, Vector};
 
-use std::fmt;
-
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Hex {
   pub coordinate: HexCoordinate,
@@ -38,76 +36,6 @@ impl HexCoordinate {
     Vector::new(
       1.5 * self.q as f32,
       f32::sqrt(3.0) * (0.5 * self.q as f32 + self.r as f32),
-    )
-  }
-}
-
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
-enum HexTerrain {
-  Aquatic,
-  Arctic,
-  Desert,
-  Forest,
-  Moutain,
-  Plain,
-  Swamp,
-}
-
-impl HexTerrain {
-  const ALL: [HexTerrain; 7] = [
-    HexTerrain::Aquatic,
-    HexTerrain::Arctic,
-    HexTerrain::Desert,
-    HexTerrain::Forest,
-    HexTerrain::Moutain,
-    HexTerrain::Plain,
-    HexTerrain::Swamp,
-  ];
-}
-
-impl fmt::Display for HexTerrain {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    write!(
-      f,
-      "{}",
-      match self {
-        HexTerrain::Aquatic => "Aquatic",
-        HexTerrain::Arctic => "Arctic",
-        HexTerrain::Desert => "Desert",
-        HexTerrain::Forest => "Forest",
-        HexTerrain::Moutain => "Mountain",
-        HexTerrain::Plain => "Plain",
-        HexTerrain::Swamp => "Swamp",
-      }
-    )
-  }
-}
-
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
-enum HexTerrainDifficulty {
-  Open,
-  Difficult,
-  GreaterDifficult,
-}
-
-impl HexTerrainDifficulty {
-  const ALL: [HexTerrainDifficulty; 3] = [
-    HexTerrainDifficulty::Open,
-    HexTerrainDifficulty::Difficult,
-    HexTerrainDifficulty::GreaterDifficult,
-  ];
-}
-
-impl fmt::Display for HexTerrainDifficulty {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    write!(
-      f,
-      "{}",
-      match self {
-        HexTerrainDifficulty::Open => "Open",
-        HexTerrainDifficulty::Difficult => "Difficult",
-        HexTerrainDifficulty::GreaterDifficult => "Greater Difficult",
-      }
     )
   }
 }
