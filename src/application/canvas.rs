76e57798f06a27bf65f23123bca8607806805c66
Siobhán Sterrett <siobhan.sterrett@gmail.com>
2021-12-17 09:56:30 -0500

    Adds euclid

--- src/application/canvas.rs
+++ src/application/canvas.rs
@@ -1,9 +1,15 @@
 use iced::canvas::{self, event, Cursor, Event, Geometry, Path, Program, Stroke};
-use iced::{mouse, Color, Rectangle, Vector};
+use iced::{mouse, Color, Point, Rectangle, Vector};
 
-use crate::hex::{Hex, HexCoordinate};
+use euclid::{Point2D, Transform2D, Vector2D};
+
+use crate::hex::{Hex, HexCoordinate, HexSpace};
 
 use std::collections::HashMap;
+
+struct HexCanvasSpace;
+type HexCanvasPoint = Point2D<f32, HexCanvasSpace>;
+type HexCanvasVector = Vector2D<f32, HexCanvasSpace>;
 
 struct Cache {
   hex_outlines: canvas::Cache,
@@ -22,6 +28,7 @@ impl Cache {
 pub struct HexCanvas<'a> {
   hexes: &'a HashMap<HexCoordinate, Hex>,
   selected: Option<HexCoordinate>,
+  transform: Transform2D<f32, HexSpace, HexCanvasSpace>,
   cache: Cache,
 }
 
@@ -30,6 +37,7 @@ impl<'a> HexCanvas<'a> {
     HexCanvas {
       hexes,
       selected: None,
+      transform: Transform2D::identity(),
       cache: Cache::new(),
     }
   }
@@ -43,45 +51,28 @@ impl<'a> Program<()> for HexCanvas<'a> {
     cursor: Cursor,
   ) -> (event::Status, Option<()>) {
     let cursor_position = if let Some(position) = cursor.position_in(&bounds) {
-      position
+      HexCanvasPoint::new(position.x, position.y)
     } else {
       return (event::Status::Ignored, None);
     };
 
     let center = bounds.center();
+    let center = HexCanvasPoint::new(center.x, center.y);
     let radius = bounds.width.min(bounds.height) / 2.0;
     let hex_radius = radius / 10.0;
-    let Vector { x, y } = center - cursor_position;
+
+    self.transform = Transform2D::scale(hex_radius, hex_radius)
+      .then_translate(HexCanvasVector::new(center.x, center.y));
 
     match event {
       Event::Mouse(mouse_event) => {
         if let mouse::Event::ButtonPressed(mouse::Button::Left) = mouse_event {
-          // if the cursor is positioned over a hex, return a Message::SelectedHex
-          let q = 2.0 * x / (3.0 * hex_radius);
-          let r = -(x + f32::sqrt(3.0) * y) / (3.0 * hex_radius);
-          let s = 0.0 - q - r;
-
-          let q_i = f32::round(q);
-          let r_i = f32::round(r);
-          let s_i = f32::round(s);
-
-          let q_d = f32::abs(q_i - q);
-          let r_d = f32::abs(r_i - r);
-          let s_d = f32::abs(s_i - s);
-
-          let mut q = q_i as i64;
-          let mut r = r_i as i64;
-          let mut s = s_i as i64;
-
-          if q_d > r_d && q_d > s_d {
-            q = 0 - r - s;
-          } else if r_d > s_d {
-            r = 0 - q - s;
-          } else {
-            s = 0 - q - r;
-          }
-
-          let coord = HexCoordinate::new(q, r, s);
+          let coord: HexCoordinate = self
+            .transform
+            .inverse()
+            .unwrap()
+            .transform_point(cursor_position)
+            .into();
 
           if self.hexes.contains_key(&coord) {
             self.selected = Some(coord);
@@ -102,25 +93,24 @@ impl<'a> Program<()> for HexCanvas<'a> {
     let radius = f32::min(bounds.width, bounds.height) / 2.0;
     let hex_radius = radius / 10.0;
 
+    let transform: Transform2D<f32, HexSpace, HexCanvasSpace> =
+      Transform2D::scale(hex_radius, hex_radius)
+        .then_translate(HexCanvasVector::new(center.x, center.y));
+
     let hex_outlines = self.cache.hex_outlines.draw(bounds.size(), |frame| {
       let hexes = Path::new(|builder| {
-        for coordinate in self.hexes.keys() {
-          let hex_center = center + coordinate.as_vector() * hex_radius;
+        for hex in self.hexes.values() {
+          let hex_center = transform.transform_point(hex.center());
 
-          let mut vertices = (0..6).into_iter().map(|i| {
-            let (sin, cos) = f32::sin_cos(i as f32 * std::f32::consts::FRAC_PI_3);
+          let mut vertices = Hex::vertices()
+            .map(|v| transform.transform_vector(v))
+            .map(|v| hex_center + v);
 
-            hex_center
-              + Vector {
-                x: hex_radius * cos,
-                y: hex_radius * sin,
-              }
-          });
-
-          builder.move_to(vertices.next().unwrap());
+          let vertex = vertices.next().unwrap();
+          builder.move_to(Point::new(vertex.x, vertex.y));
 
           for vertex in vertices {
-            builder.line_to(vertex);
+            builder.line_to(Point::new(vertex.x, vertex.y));
           }
 
           builder.close();
@@ -145,23 +135,20 @@ impl<'a> Program<()> for HexCanvas<'a> {
             ..Default::default()
           };
 
+          let hex = self.hexes.get(&coord).unwrap();
+
           let path = Path::new(|builder| {
-            let hex_center = center + coord.as_vector() * hex_radius;
+            let hex_center = transform.transform_point(hex.center());
 
-            let mut vertices = (0..6).into_iter().map(|i| {
-              let (sin, cos) = f32::sin_cos(i as f32 * std::f32::consts::FRAC_PI_3);
+            let mut vertices = Hex::vertices()
+              .map(|v| transform.transform_vector(v))
+              .map(|v| hex_center + v);
 
-              hex_center
-                + Vector {
-                  x: hex_radius * cos,
-                  y: hex_radius * sin,
-                }
-            });
-
-            builder.move_to(vertices.next().unwrap());
+            let vertex = vertices.next().unwrap();
+            builder.move_to(Point::new(vertex.x, vertex.y));
 
             for vertex in vertices {
-              builder.line_to(vertex);
+              builder.line_to(Point::new(vertex.x, vertex.y));
             }
 
             builder.close();
--- src/hex.rs
+++ src/hex.rs
@@ -3,7 +3,13 @@ use serde_json::Value;
 
 use iced::{Point, Vector};
 
+use euclid::{Point2D, Vector2D};
+
 use std::collections::HashMap;
+
+pub struct HexSpace;
+pub type HexPoint = Point2D<f32, HexSpace>;
+pub type HexVector = Vector2D<f32, HexSpace>;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Hex {
@@ -11,6 +17,30 @@ pub struct Hex {
 
   #[serde(flatten)]
   pub data: HashMap<String, Value>,
+}
+
+impl Hex {
+  pub fn new(coordinate: HexCoordinate) -> Hex {
+    Hex {
+      coordinate,
+      data: HashMap::new(),
+    }
+  }
+
+  pub fn center(&self) -> HexPoint {
+    HexPoint::new(
+      1.5 * self.coordinate.q as f32,
+      f32::sqrt(3.0) * (0.5 * self.coordinate.q as f32 + self.coordinate.r as f32),
+    )
+  }
+
+  pub fn vertices() -> impl Iterator<Item = HexVector> {
+    (0..6).into_iter().map(move |i| {
+      let (sin, cos) = f32::sin_cos(i as f32 * std::f32::consts::FRAC_PI_3);
+
+      HexVector::new(cos, sin)
+    })
+  }
 }
 
 #[derive(Copy, Clone, Debug, Hash, PartialEq, Eq, Serialize, Deserialize)]
@@ -41,5 +71,35 @@ impl HexCoordinate {
       1.5 * self.q as f32,
       f32::sqrt(3.0) * (0.5 * self.q as f32 + self.r as f32),
     )
+  }
+}
+
+impl From<HexPoint> for HexCoordinate {
+  fn from(point: HexPoint) -> HexCoordinate {
+    let q = -2.0 * point.x / 3.0;
+    let r = (point.x + f32::sqrt(3.0) * point.y) / 3.0;
+    let s = 0.0 - q - r;
+
+    let q_i = f32::round(q);
+    let r_i = f32::round(r);
+    let s_i = f32::round(s);
+
+    let q_d = f32::abs(q_i - q);
+    let r_d = f32::abs(r_i - r);
+    let s_d = f32::abs(s_i - s);
+
+    let mut q = q_i as i64;
+    let mut r = r_i as i64;
+    let mut s = s_i as i64;
+
+    if q_d > r_d && q_d > s_d {
+      q = 0 - r - s;
+    } else if r_d > s_d {
+      r = 0 - q - s;
+    } else {
+      s = 0 - q - r;
+    }
+
+    HexCoordinate { q, r, s }
   }
 }
